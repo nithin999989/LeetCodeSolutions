67e719abb0072570b290c0ad87c98c45f3af5abf
Nithin <82808965+Nithin108@users.noreply.github.com>
2022-06-14 23:18:02 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (67.56%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,21 +1,15 @@
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int a[]=new int[nums.length];
-       int  j=0;
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                a[j]=nums[i];
-                count++;
-                j++;
-                
-            }
-            
-            
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=a[i];
-        
-    }return count;
+public class Solution {
+
+public int removeElement(int[] A, int elem) {
+   int m = 0;    
+   for(int i = 0; i < A.length; i++){
+       
+       if(A[i] != elem){
+           A[m] = A[i];
+           m++;
+       }
+   }
+   
+   return m;
 }
 }
