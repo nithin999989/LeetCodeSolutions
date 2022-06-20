a257bb2007651126d9078c502ffe41dd5aac25b2
Nithin <82808965+Nithin108@users.noreply.github.com>
2022-06-20 20:07:36 +0530

    Time: 14 ms (49.47%), Space: 67.7 MB (78.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,11 +1,36 @@
 class Solution {
-    public boolean containsDuplicate(int[] a) {
-        Arrays.sort(a);
-        for(int i=0;i<a.length-1;i++){
-            if(a[i]==a[i+1]){
-                return true;
-            }
+    public boolean containsDuplicate(int[] arr) {
+         int n = arr.length;
+        
+        //IF ONLY ONE ELEMENT RETURN FALSE
+        if(n == 1){return false;}
+        
+        //FOR STORING THE ELEMENTS
+        HashSet<Integer> set = new HashSet<>();
+        
+        //O OT N IF ANY ELEMENT ALLREADY PRESENT
+        //RETURN TRUE
+        for(int i =0; i<n; i++)
+        {
+            if(set.contains(arr[i])){return true;}
+            set.add(arr[i]);
         }
-    return false;
+        
+        //IF NO DUPLICATES PRESENT RETURN FALSE
+        return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
 }
