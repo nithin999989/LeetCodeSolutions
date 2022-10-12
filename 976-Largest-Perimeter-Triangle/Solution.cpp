374e546592870c5c72c122ed42d49af2f6933f23
Amit Das <91092039+das-amit@users.noreply.github.com>
2022-10-12 16:31:09 +0530

    Added cpp solution

---
+++ 976-Largest-Perimeter-Triangle/Solution.cpp
@@ -0,0 +1,7 @@
+int Solution(vector<int>& A) {
+        sort(A.begin(), A.end());
+        for (int i = A.size() - 1 ; i > 1; --i)
+            if (A[i] < A[i - 1] + A[i - 2])
+                return A[i] + A[i - 1] + A[i - 2];
+        return 0;
+    }
