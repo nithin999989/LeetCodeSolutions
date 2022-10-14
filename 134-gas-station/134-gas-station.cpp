bef8cfc23d09e85ed86f4984390c8a9456c2959c
iamnimesh <89510282+iamnimesh@users.noreply.github.com>
2022-10-14 20:00:28 +0530

    Update 134-gas-station.cpp

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        
-        int start=0;
-        int deficit=0;
-        int balance=0;
-        
-        for(int i=0;i<gas.size();i++)
-        {
-            balance += gas[i]-cost[i];
-            
-              // If at any point our balance/ current fuel is negative that means we can't come               //to the i'th petrol pump from the previous pump.So we'll make the i'th pump as                //the start point .we'll also make the current fuel to be 0 as we're                            //starting freshly.
-            if(balance<0){
-                start=i+1;
-                deficit += balance;
-                balance = 0;
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+	{
+        int n = gas.size();
+        int total(0), subsum(INT_MAX), start(0);
+        for(int i = 0; i < n; ++i)
+		{
+            total += gas[i] - cost[i];
+            if(total < subsum) 
+			{
+                subsum = total;
+                start  = i + 1;
             }
         }
-        //to check if we can cover the deficit that we left behind (if we did) after the end of         //list
-        if(deficit + balance >=0) return start;
-        return -1;
-        
+        return (total < 0) ?  -1 : (start % n); 
     }
 };
