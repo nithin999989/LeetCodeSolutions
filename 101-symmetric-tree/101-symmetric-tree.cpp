8e5e27a4f428c22375df7b7cffdb1c2ae46e8556
iamnimesh <89510282+iamnimesh@users.noreply.github.com>
2022-10-14 19:59:03 +0530

    Update 101-symmetric-tree.cpp

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -1,29 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    bool helper(TreeNode*p,TreeNode*q)
-    {
-        if(!p or !q) return p==q;
+    bool isSymmetric(TreeNode* root) {
         
-        if(p->val != q->val) return false;
-        return helper(p->left,q->right) && helper(p->right,q->left);
+        if(root==NULL) return true; //Tree is empty
+        
+        return isSymmetricTest(root->left,root->right);
     }
     
-    bool isSymmetric(TreeNode* root)
-    {
-        if(!root) return false;
-        return helper(root->left,root->right);
+    bool isSymmetricTest(TreeNode* p , TreeNode* q){
+        if(p == NULL && q == NULL) //left & right node is NULL 
+            return true; 
         
+        else if(p == NULL || q == NULL) //one of them is Not NULL
+            return false; 
+        
+        else if(p->val!=q->val) 
+            return false;
+        
+        return isSymmetricTest(p->left,q->right) && isSymmetricTest(p->right,q->left); //comparing left subtree's left child with right subtree's right child --AND-- comparing left subtree's right child with right subtree's left child
     }
 };
