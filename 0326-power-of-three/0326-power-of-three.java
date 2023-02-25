d8ff96b775a331734428776eeafae81e730e1e6b
M N S R NITHIN <82808965+nithin999989@users.noreply.github.com>
2023-02-25 16:22:37 +0530

    Time: 16 ms (38.37%), Space: 42.6 MB (16.36%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPowerOfThree(int n) {
          double a = Math.log10(n)/ Math.log10(3);
       
-        return Math.abs(a - Math.round(a)) < 1e-10;
+        return a%1==0;
     
        
         
