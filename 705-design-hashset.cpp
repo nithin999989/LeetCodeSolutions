6f859b92d5b82de3ed6d8ce86e6722ec1a3ca58e
Naitik Sharma <73165529+Naitik234@users.noreply.github.com>
2023-10-10 13:33:12 +0530

    Create 705-design-hashset.cpp
    hashset using cpp

---
+++ 705-design-hashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+public:
+    
+    vector<int> m;
+    int size;
+    MyHashSet() {
+        size = 1e6+1;
+        m.resize(size);
+    }
+    
+    void add(int key) {
+        m[key]=1;
+    }
+    
+    void remove(int key) {
+        m[key]=0;
+    }
+    
+    bool contains(int key) {
+        return m[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
