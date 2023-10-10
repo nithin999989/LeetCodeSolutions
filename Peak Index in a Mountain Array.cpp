356a3b212e1a8494aac67444cd71beee494f4ae4
Ashish Chanchal <akchanchal2002@gmail.com>
2023-10-10 12:14:11 +0000

    2 problems solution added

---
+++ Peak Index in a Mountain Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0,e=arr.size()-1;
+        int mid =  s + (e-s)/2;
+        while(s<e){
+            if(arr[mid]<arr[mid+1])
+            {
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+            mid=s + (e-s)/2;
+        }
+        return s;
+    }
+};
