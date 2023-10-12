293bf68dc89f2949e5cbffb4dadefcae1c24cd82
Sakshi Gupta <129653737+sakshii281@users.noreply.github.com>
2023-10-12 14:49:42 +0530

    Create 242_Valid_Anagram.cpp

---
+++ 242_Valid_Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s==t){
+            return true;
+        }
+        return false;
+
+
+   
+    }
+};
