ac63d3a29ea93e8ea506ac08982e4243b71b9075
Nataly Martins <66051162+tetsab@users.noreply.github.com>
2023-10-16 13:57:49 -0300

    Update 0165-compare-version-numbers.java

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,11 +1,11 @@
-class Solution {
+public class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+        String[] ver1 = version1.split("\\.");
+        String[] ver2 = version2.split("\\.");
         int i = 0;
-        for (; i < v1.length && i < v2.length; i ++) {
-            int v1 = Integer.parseInt(v1[i]);
-            int v2 = Integer.parseInt(v2[i]);
+        for (; i < ver1.length && i < ver2.length; i ++) {
+            int v1 = Integer.parseInt(ver1[i]);
+            int v2 = Integer.parseInt(ver2[i]);
             if (v1 > v2) {
                 return 1;
             }
@@ -13,16 +13,16 @@ class Solution {
                 return -1;
             }
         }
-        if (i < v1.length) {
-            for (;i < v1.length;i++) {
-                if (Integer.parseInt(v1[i]) != 0) {
+        if (i < ver1.length) {
+            for (;i < ver1.length;i++) {
+                if (Integer.parseInt(ver1[i]) != 0) {
                     return 1;
                 }
             }
         }
-        if (i < v2.length) {
-            for (;i < v2.length;i++) {
-                if (Integer.parseInt(v2[i]) != 0) {
+        if (i < ver2.length) {
+            for (;i < ver2.length;i++) {
+                if (Integer.parseInt(ver2[i]) != 0) {
                     return -1;
                 }
             }
