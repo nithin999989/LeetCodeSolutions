61ab6f8c99d3808ad993557533d0ec0627710f51
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-10-17 20:51:21 +0530

    Create 1720-decode-xored-array.cpp

---
+++ 1720-decode-xored-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int>ans;
+        ans.push_back(first);
+        for(int i=0;i<encoded.size();i++)
+        {
+            first=encoded[i]^first;
+            ans.push_back(first);
+        }
+        return ans;
+    }
+};
